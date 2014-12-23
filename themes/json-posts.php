e4d85e78233fa569497db3dd800ef90890067a70
William Garcia <william.garcia@sideways-nyc.com>
2014-12-22 19:34:18 -0500

    Adding theme auxiliary file

--- themes/json-posts.php
+++ themes/json-posts.php
@@ -1,19 +1,20 @@
 <?php
-//Auxiliary template file you can drop in your theme file to hijack the JSON spit-out.
+/* 
+Auxiliary template file you can drop in your theme file to hijack the JSON spit-out.
+You can modify and extend in conjunction with the plugin to format your JSON as needed.
+*/
 global $wp_query, $post;
 $post_data = array();
 
-if ($wp_query->have_posts())
-	{
+if ($wp_query->have_posts()){
 	$key = 0;
-	while ($wp_query->have_posts())
-		{
+	while ($wp_query->have_posts()){
 		the_post();
 		$post_id = get_the_ID();
 		$post_data[$key] = get_post();
 		$key++;
-		}
+		
 	}
+}
 
 echo json_encode($post_data);
-?>
